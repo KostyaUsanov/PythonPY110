6e1005fc6dec758dd4164156110c7a6e9bf5cccf
konusa <u-kostya@mail.ru>
2024-01-09 21:02:07 +0300

    lab6

--- logic/services.py
+++ logic/services.py
@@ -126,68 +126,85 @@ def add_user_to_cart(request, username: str) -> None:
             json.dump(cart_users, f)
 
 
-# def view_in_wishlist(request) -> dict:
-#     """
-#     Просматривает содержимое cart.json
-#
-#     :return: Содержимое 'cart.json'
-#     """
-#     if os.path.exists('cart.json'):  # Если файл существует
-#         with open('cart.json', encoding='utf-8') as f:
-#             return json.load(f)
-#
-#     user = get_user(request).username  # Получаем авторизированного пользователя
-#     wishlist = {user: {'products': {}}}
-#     with open('cart.json', mode='x', encoding='utf-8') as f:  # Создаём файл и записываем туда пустую корзину
-#         json.dump(wishlist, f)
-#
-#     return wishlist
-#
-#
-# def add_to_wishlist(request, id_product: str) -> bool:
-#     """
-#     Добавляет продукт в избранное. Если в избранном нет данного продукта, то добавляет его с количеством равное 1.
-#     Если в избранном есть такой продукт, то добавляет количеству данного продукта + 1.
-#
-#     :param id_product: Идентификационный номер продукта в виде строки.
-#     :return: Возвращает True в случае успешного добавления, а False в случае неуспешного добавления(товара по id_product
-#     не существует).
-#     """
-#     wishlist_users = view_in_wishlist(request)
-#     wishlist = wishlist_users[get_user(request).username]
-#
-#     if id_product not in wishlist['products']:
-#         wishlist['products'][id_product] = 1
-#         return False
-#     if id_product not in DATABASE:
-#         return False
-#
-#     with open('cart.json', mode='w', encoding='utf-8') as f:  # Создаём файл и записываем туда пустое избранное
-#         json.dump(wishlist_users, f)
-#
-#     return True
-#
-#
-# def remove_from_wishlist(request, id_product: str) -> bool:
-#     """
-#     Добавляет позицию продукта из избранного. Если в избранном есть такой продукт, то удаляется ключ в словаре
-#     с этим продуктом.
-#
-#     :param id_product: Идентификационный номер продукта в виде строки.
-#     :return: Возвращает True в случае успешного удаления, а False в случае неуспешного удаления(товара по id_product
-#     не существует).
-#     """
-#     wishlist_users = view_in_wishlist(request)
-#     wishlist = wishlist_users[get_user(request).username]
-#
-#
-#     if id_product in wishlist['products']:
-#         wishlist['products'].pop(id_product)
-#         with open('cart.json', mode='w', encoding='utf-8') as f:  # Создаём файл и записываем туда пустую корзину
-#             json.dump(wishlist_users, f)
-#         return True
-#     return False
-#
+
+def view_in_wishlist(request) -> dict:
+    """
+    Просматривает содержимое wishlist.json
+
+    :return: Содержимое 'wishlist.json'
+    """
+    if os.path.exists('wishlist.json'):  # Если файл существует
+        with open('wishlist.json', encoding='utf-8') as f:
+            return json.load(f)
+
+    user = get_user(request).username  # Получаем авторизированного пользователя
+    wishlist = {user: {'products': []}}
+    with open('wishlist.json', mode='x', encoding='utf-8') as f:  # Создаём файл и записываем туда пустое избранное
+        json.dump(wishlist, f)
+
+    return wishlist
+
+
+def add_to_wishlist(request, id_product: str) -> bool:
+    """
+    Добавляет продукт в избранное. Если в избранном нет данного продукта, то добавляет его с количеством равное 1.
+    Если в избранном есть такой продукт, то добавляет количеству данного продукта + 1.
+
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного добавления, а False в случае неуспешного добавления(товара по id_product
+    не существует).
+    """
+    wishlist_users = view_in_wishlist(request)
+    wishlist = wishlist_users[get_user(request).username]
+
+    if id_product not in wishlist['products']:
+        wishlist['products'].append(id_product)
+        with open('wishlist.json', mode='w', encoding='utf-8') as f:  # Создаём файл и записываем туда пустое избранное
+            json.dump(wishlist_users, f)
+        return True
+
+    if id_product not in DATABASE:
+        return False
+
+
+
+
+
+def remove_from_wishlist(request, id_product: str) -> bool:
+    """
+    Добавляет позицию продукта из избранного. Если в избранном есть такой продукт, то удаляется ключ в словаре
+    с этим продуктом.
+
+    :param id_product: Идентификационный номер продукта в виде строки.
+    :return: Возвращает True в случае успешного удаления, а False в случае неуспешного удаления(товара по id_product
+    не существует).
+    """
+    wishlist_users = view_in_wishlist(request)
+    wishlist = wishlist_users[get_user(request).username]
+
+
+    if id_product in wishlist['products']:
+        wishlist['products'].remove(id_product)
+        with open('wishlist.json', mode='w', encoding='utf-8') as f:
+            json.dump(wishlist_users, f)
+        return True
+    return False
+
+def add_user_to_wishlist(request, username: str) -> None:
+    """
+    Добавляет пользователя в базу данных избранного, если его там не было.
+
+    :param username: Имя пользователя
+    :return: None
+    """
+    wishlist_users = view_in_wishlist(request)  # Чтение всей базы корзин
+
+    wishlist = wishlist_users.get(username)  # Получение корзины конкретного пользователя
+
+    if not wishlist:  # Если пользователя до настоящего момента не было в корзине, то создаём его и записываем в базу
+        with open('wishlist.json', mode='w', encoding='utf-8') as f:
+            wishlist_users[username] = {'products': {}}
+            json.dump(wishlist_users, f)
 
 
 
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import wishlist_view
+from .views import wishlist_view, wishlist_add_view, wishlist_del_view
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', wishlist_view, name="wishlist_view"),
+    path('remove/<str:id_product>', wishlist_del_view, name="remove_from_wishlist"),
+    path('add/<str:id_product>', wishlist_add_view, name="wishlist_add_view")
+
 ]
